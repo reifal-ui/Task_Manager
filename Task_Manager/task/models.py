fc55d49c45c46ee02f072a4ac1d65ffd505bab94
fadlifauzi2 <fadlifauzi021106@gmail.com>
2026-04-17 10:08:22 +0700

    Penambahan model dan form, serta app task

---
+++ Task_Manager/task/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add= True)
+    def is_end_date(self):
+        return datetime.now > self.end_date
